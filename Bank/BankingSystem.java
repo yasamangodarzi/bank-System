0e6e7bbc485201463b988e08047555bd80ddce52
yasamangodarzi <78849693+yasamangodarzi@users.noreply.github.com>
2021-04-12 08:03:57 -0700

    Add files via upload

---
+++ Bank/BankingSystem.java
@@ -0,0 +1,114 @@
+package com.company;
+import java.util.Iterator;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class BankingSystem {
+    ArrayList<User> users;
+    ArrayList<Account> accounts;
+
+    public BankingSystem() {
+        users = new ArrayList<>();
+        accounts = new ArrayList<>();
+    }
+
+    public void displayUsers() {
+        int i = 1;
+        for (User str : users) {
+            System.out.print("User " + i + ":");
+            str.printUserData();
+            i++;
+        }
+    }
+
+    public void displayAccounts() {
+        int i = 1;
+        for (Account str : accounts) {
+            System.out.print("Account " + i + ":");
+            str.printAccountData();
+            i++;
+        }
+    }
+
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    public void removeUser(User user) {
+        users.remove(user);
+    }
+
+    public void addAccount(Account account) {
+        accounts.add(account);
+    }
+
+    public void removeAccount(Account account) {
+        accounts.remove(account);
+    }
+    public Account findAccount(String serial) {
+        for (Account str : accounts) {
+            if (str.getSerial().equals(UUID.fromString(serial))) {
+                return str;
+            }
+        }
+       return null;
+    }
+
+    public User findUser(String id,String password) {
+        for (User str : users) {
+            if (str.getPassword().equals(password))
+            {
+                if (str.getId().equals(id))
+                {
+                    return str;
+                }
+            }
+        }
+       return null;
+    }
+    public User findUser(String id) {
+        for (User str : users) {
+                if (str.getId().equals(id))
+                {
+                    return str;
+                }
+        }
+        return null;
+    }
+    public void register(User user)
+    {
+        Iterator<User> it = users.iterator();
+        boolean existUser = false;
+        while(it.hasNext()) {
+            User i = it.next();
+            if(i.getId().equals(user.getId())) {
+                System.out.println("user already exists.");
+                existUser=true;
+            }
+        }
+        if (existUser==false)
+        {
+            users.add(user);
+            System.out.println("user created.");
+        }
+    }
+    public void login(String id, String password)
+    {
+        Iterator<User> it = users.iterator();
+        while(it.hasNext()) {
+            User i = it.next();
+            if(i.getId().equals(id)) {
+                if(i.getPassword().equals(password))
+                {
+                    System.out.println("Logged in.");
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ Bank/User.java
@@ -0,0 +1,134 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class User {
+
+   private String firstName;
+   private String lastName;
+   private   String id;
+   private String password;
+   ArrayList<Account> accounts;
+
+    public User(String firstName, String lastName, String id, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+        this.password = password;
+        accounts =new ArrayList<>();
+    }
+
+    public void printAllAvailableAccounts()
+    {
+        int i=1;
+        for (Account str : accounts)
+        {
+            System.out.print("Account " + i +":");
+            str.printAccountData();
+            i++;
+        }
+    }
+   public void printUserData()
+   {
+      // System.out.println(user.toString());
+       System.out.println("firstName='" + firstName + '\'' +
+               ", lastName='" + lastName + '\'' +
+               ", id='" + id + '\'');
+   }
+   public void addAccount(Account account)
+   {
+       accounts.add(account);
+   }
+   public void removeAccount(Account account)
+   {
+       accounts.remove(account);
+   }
+   public void checkBalance(Account account)
+   {
+       System.out.println("balance :" +account.getBalance());
+   }
+   public void deposit(Account account, int amount)
+   {
+       if (accounts.contains(account))
+       {
+           int balance=account.getBalance();
+           balance+=amount;
+           account.updateBalance(balance);
+           Transaction transaction=new Transaction(amount);
+           account.addTransaction(transaction);
+
+
+       }else
+       {
+           System.out.println("your account incorract");
+       }
+
+   }
+   public void withdrawal(Account account, int amount)
+   {
+       if (accounts.contains(account))
+       {
+           int balance=account.getBalance();
+           balance-=amount;
+           if (balance<0)
+           {
+               System.out.println("Destination account doesn’t exist or there is not enough money \n" +
+                       "in your account.");
+           }
+           else {
+               account.updateBalance(balance);
+               Transaction transaction = new Transaction((amount*-1));
+               account.addTransaction(transaction);
+               System.out.println("Completed");
+           }
+       }else
+       {
+           System.out.println("Destination account doesn’t exist or there is not enough money \n" +
+                   "in your account.");
+       }
+
+   }
+   public void transfer(Account srcAccount, Account destAccount, int amount)
+   {
+       if (accounts.contains(srcAccount))
+       {
+           int balanceSrcAccount=srcAccount.getBalance();
+           int balanceDestAccount=destAccount.getBalance();
+           balanceSrcAccount-=amount;
+           balanceDestAccount+=amount;
+           if (balanceSrcAccount<0)
+           {
+               System.out.println("Destination account doesn’t exist or there is not enough money \n" +
+                       "in your account.");
+           }else{
+               srcAccount.updateBalance(balanceSrcAccount);
+               destAccount.updateBalance(balanceDestAccount);
+               Transaction transactionSrc = new Transaction((amount*-1));
+               Transaction transactionDest = new Transaction(amount);
+               srcAccount.addTransaction(transactionSrc);
+               destAccount.addTransaction(transactionDest);
+               System.out.println("Completed");
+           }
+       }else
+       {
+           System.out.println("Destination account doesn’t exist or there is not enough money \n" +
+                   "in your account.");
+       }
+   }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
